5bb0fab19aee4dc3162c8bca9671649e9458b915
Zero <63326326+ZeroMeOut@users.noreply.github.com>
2023-07-24 21:16:44 +0800

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -1,7 +1,9 @@
 import requests
 import pprint
 import json
+import time
 
+# Getting Geo Location, longitude and latitude
 address = "London"
 geocodeResponse = requests.get(f"https://geocode.maps.co/search?q={address}")
 geocodeResponse.raise_for_status()
@@ -21,4 +23,18 @@ def cleanResponse(response: list):
         list_of_results.append(dict)
     return list_of_results
 
-pprint.pprint(cleanResponse(jsonGeocodeResponse))
+data = cleanResponse(jsonGeocodeResponse)
+
+location = data[0]
+lat = location['lat']
+lon = location['lon']
+API_key = '5bfa7c20f7c28654ffefacc67dbc0913'
+
+while True:
+    openweathermapResponse = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}')
+    jsonOpenweathermapResponse = json.loads(openweathermapResponse.text)
+
+    pprint.pprint(jsonOpenweathermapResponse)
+
+    time.sleep(10)
+
