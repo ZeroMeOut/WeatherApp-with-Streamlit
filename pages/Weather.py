22b1ee30798f01990615935db93a9dbd857d9e25
Zero <63326326+ZeroMeOut@users.noreply.github.com>
2023-08-07 07:36:47 +0800

    8/7/2023

--- pages/Weather.py
+++ pages/Weather.py
@@ -1,38 +1,22 @@
 import streamlit as st
 from json import loads
-from kafka import KafkaConsumer
-
-# Cleans up data from the consumer
-def data_cleanup(message):
-    lon = message['coord']['lon']
-    lat = message['coord']['lat']
-
-    weather = message['weather'][0]['main']
-    weather_desc = message['weather'][0]['description']
-
-    temp = message['main']['temp'] - 273.15
-    feels_like = message['main']['feels_like'] - 273.15
-    temp_min = message['main']['temp_min'] - 273.15
-    temp_max = message['main']['temp_max'] - 273.15
-    humidity = message['main']['humidity']
-
-    clouds = message['clouds']['all']
-
-    return lon, lat, weather, weather_desc, temp, feels_like, temp_min, temp_max, humidity, clouds
+from kafka import KafkaConsumer, TopicPartition
 
 
 consumer = KafkaConsumer(
-    'dataflow',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset= 'earliest',
     enable_auto_commit=True,
     group_id='my-group',
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+    value_deserializer=lambda x: loads(x.decode('utf-8')),
+    )
 
+partition = TopicPartition('dataflow', 0)
+consumer.assign([partition])
+end_offset = consumer.end_offsets([partition])
+consumer.seek(partition,list(end_offset.values())[0]-1)
 
-
-for message in consumer:
-    
+for message in consumer: 
     st.write(message.value)
 
 
--- producer.py
+++ producer.py
@@ -4,6 +4,24 @@ from time import sleep
 from kafka import KafkaProducer
 from json import dumps
 
+def data_cleanup(message):
+    lon = message['coord']['lon']
+    lat = message['coord']['lat']
+
+    weather = message['weather'][0]['main']
+    weather_desc = message['weather'][0]['description']
+
+    temp = message['main']['temp'] - 273.15
+    feels_like = message['main']['feels_like'] - 273.15
+    temp_min = message['main']['temp_min'] - 273.15
+    temp_max = message['main']['temp_max'] - 273.15
+    humidity = message['main']['humidity']
+
+    clouds = message['clouds']['all']
+
+    return lon, lat, weather, weather_desc, temp, feels_like, temp_min, temp_max, humidity, clouds
+
+
 
 with open('data.json') as file:
     data = json.load(file)
@@ -18,8 +36,10 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
 
 while True:
     openweathermapResponse = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}')
-    producer.send('dataflow', value=openweathermapResponse.text)
-    producer.flush()
-    sleep(1000)
+    lon, lat, weather, weather_desc, temp, feels_like, temp_min, temp_max, humidity, clouds = data_cleanup(json.loads(openweathermapResponse.text))
+
+    producer.send('dataflow', value=lon)
+    sleep(5)
+
         
     
